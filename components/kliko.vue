707770ce821ed1238755ba3e66e597456f566de1
BelindaMyteberi <belindamyteberi@gmail.com>
2019-04-22 15:06:17 +0200

    --add: nuxtjs package

---
+++ components/kliko.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header-container">
+    <button @click="getValue">Kliko</button>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        display: 'Shfaq'
+      }
+    },
+    methods: {
+      getValue () {
+        console.log(this.display)
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+   <kliko-component></kliko-component>
+  </div>
+</template>
+<script>
+  import klikoComponent from '@/components/kliko'
+  export default {
+    components: {
+      klikoComponent
+    }
+  }
+</script>
